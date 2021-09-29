fb83dece7b1d381622d97e7d37773b7478ee9b26
Justice, Aidan <Aidan.Justice@mines.sdsmt.edu>
2021-09-29 16:14:17 -0600

    m0050 - [ci skip] - finished reading and writing to file

--- m0050/m0050.cpp
+++ m0050/m0050.cpp
@@ -1,6 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <iomanip>
 using namespace std;
 
 struct empData
@@ -16,6 +19,12 @@ int main(int argc, char** argv)
 {
     ifstream fin;
     ofstream fout;
+    vector<empData> vemployee;
+    string header;
+    string line;
+    empData employee;
+    int i = 0;
+    long long pos;
 
     if (argc != 4)
     {
@@ -38,13 +47,52 @@ int main(int argc, char** argv)
         return 0;
     }
 
-    if ( (strcmp( argv[3], "-i" ) != 0) && (strcmp( argv[3], "-n" ) != 0) &&
-         (strcmp( argv[3], "-s" ) != 0) )
+    if ((strcmp(argv[3], "-i") != 0) && (strcmp(argv[3], "-n") != 0) &&
+        (strcmp(argv[3], "-s") != 0))
     {
-        cout << "Invalid sort method:" << endl << "-i - sort by id" << endl
+        cout << "Invalid Sort Method:" << endl << "-i - sort by id" << endl
              << "-n - sort by name" << endl << "-s - sort by salary";
+        return 0;
     }
 
+    //Read in header
+    getline(fin, header);
+
+    //Read in employee data
+    i = 0;
+    while ( getline( fin, line ) )
+    {
+        pos = line.find(",");
+        employee.id = stoi(line.substr(0, pos));
+        line.erase(0, pos + 1);
+
+        pos = line.find(",");
+        employee.firstName = line.substr(0, pos);
+        line.erase(0, pos + 1);
+
+        pos = line.find(",");
+        employee.lastName = line.substr(0, pos);
+        line.erase(0, pos + 1);
+
+        pos = line.find(",");
+        employee.salary = stod(line.substr(0, pos));
+        line.erase(0 , pos + 1);
+
+        employee.bonus = stod(line);
+
+        vemployee.push_back(employee);
+    }
+
+    //Output to file
+    fout << header << endl;
+    fout << setprecision(2) << fixed << showpoint;
+    
+    for (i = 0; i < vemployee.size(); i++)
+    {
+        fout << vemployee[i].id << "," << vemployee[i].firstName << ","
+            << vemployee[i].lastName << "," << vemployee[i].salary
+            << "," << vemployee[i].bonus << endl;
+    }
 
     return 0;
 }
