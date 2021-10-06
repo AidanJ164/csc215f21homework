a11afd48e7b1a96d95002a839123fda2a3fc5513
Justice, Aidan <Aidan.Justice@mines.sdsmt.edu>
2021-10-05 23:02:48 -0600

    m0060 - [ci skip] - finished function, going to do more test cases.

--- m0060/newton.cpp
+++ m0060/newton.cpp
@@ -3,10 +3,14 @@
 double newtonApprox(double start, function <double(double)> f,
                     function <double(double)> fP, double epsilon)
 {
-    if ( fabs( start ) )
+    if ( fabs( f(start) ) < epsilon )
     {
         return start;
     }
 
+    start = start - ( f( start ) / fP( start ) );
+    
+    start = newtonApprox(start, f, fP, epsilon);
 
+    return start;
 }
